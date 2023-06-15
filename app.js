7d16570adf3fcb996b63d198623a3c31055c1aa7
Michael Perkins <mperkins1995@gmail.com>
2023-06-14 21:39:21 -0400

    handle errors

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const BaseURL = "https://api.weatherapi.com/v1/current.json?key=";
 const APIKey = "6336a6958c58416fbed203528230905&q=";
 
 function getWeather(city) {
-  const data = fetch(BaseURL + APIKey + city, { mode: "cors" })
+  fetch(BaseURL + APIKey + city, { mode: "cors" })
+    .then(handleErrors)
     .then((response) => response.json())
-    .then((response) => processData(response))
+    .then(processData)
     .catch((err) => console.log(err));
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
 }
 
 function processData(data) {
   const { name, country } = data.location;
   const { temp_f, feelslike_f, wind_mph } = data.current;
-  console.log( name, country, temp_f, feelslike_f, wind_mph );
+  console.log(name, country, temp_f, feelslike_f, wind_mph);
   return { name, country, temp_f, feelslike_f, wind_mph };
 }
 
-getWeather("Dubai");
+const btn = document.getElementById("submit-btn");
+const input = document.getElementById("city-input");
+const form = document.getElementById("weather-form");
+
+form.addEventListener("submit", (e) => handleSubmit(e));
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const data = getUserInput();
+  getWeather(data.toString());
+}
+
+function getUserInput() {
+  return document.getElementById("city-input").value;
+}
