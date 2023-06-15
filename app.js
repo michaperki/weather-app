1f7052ae08a80ab16d8d4f1fa75ce3cb9b5c8411
Michael Perkins <mperkins1995@gmail.com>
2023-06-14 22:52:46 -0400

    output weather information

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const BaseURL = "https://api.weatherapi.com/v1/current.json?key=";
 const APIKey = "6336a6958c58416fbed203528230905&q=";
 
 function getWeather(city) {
-  fetch(BaseURL + APIKey + city, { mode: "cors" })
+  return fetch(BaseURL + APIKey + city, { mode: "cors" })
     .then(handleErrors)
     .then((response) => response.json())
     .then(processData)
@@ -10,10 +10,10 @@ function getWeather(city) {
 }
 
 function handleErrors(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response;
+  if (!response.ok) {
+    throw Error("user must enter a valid city name");
+  }
+  return response;
 }
 
 function processData(data) {
@@ -23,18 +23,38 @@ function processData(data) {
   return { name, country, temp_f, feelslike_f, wind_mph };
 }
 
-const btn = document.getElementById("submit-btn");
-const input = document.getElementById("city-input");
 const form = document.getElementById("weather-form");
-
 form.addEventListener("submit", (e) => handleSubmit(e));
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
-  const data = getUserInput();
-  getWeather(data.toString());
+  const userInput = getUserInput();
+  const weatherData = await getWeather(userInput);
+  displayWeather(weatherData);
 }
 
 function getUserInput() {
   return document.getElementById("city-input").value;
+}
+
+function displayWeather(data) {
+  const outputContainer = document.getElementById("output-container");
+  clearChildNodes(outputContainer);
+  const title = document.createElement("h1");
+  const temp = document.createElement("p");
+  const wind = document.createElement("p");
+
+  title.innerText = `${data.name}, ${data.country}`;
+  temp.innerText = `temp: ${data.temp_f} degrees fahrenheit\nfeels like: ${data.feelslike_f} degrees fahrenheit`
+  wind.innerText = `wind: ${data.wind_mph} miles per hour`
+
+  outputContainer.appendChild(title);
+  outputContainer.appendChild(temp);
+  outputContainer.appendChild(wind);
+}
+
+function clearChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
